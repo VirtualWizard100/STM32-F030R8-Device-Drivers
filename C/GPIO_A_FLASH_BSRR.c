b8d3f4a659c1680149bbc5c2c9cefa4e215b43a1
VirtualWizard100 <139145827+VirtualWizard100@users.noreply.github.com>
2026-03-10 21:41:08 -0400

    Add files via upload

---
+++ C/GPIO_A_FLASH_BSRR.c
@@ -0,0 +1,34 @@
+#define BASE_ADDRESS 0x48000000UL
+#define RCC 0x40021000UL
+
+#define GPIO_A_MODER BASE_ADDRESS
+#define RCC_AHBENR (RCC + 0x14UL)
+#define GPIO_BSRR (BASE_ADDRESS + 0x18UL)
+
+/* RCC_AHBENR Values */
+#define GPIO_A_CLOCK_ENABLE (0x1UL << 17)
+
+/* GPIO_BSRR Values */
+#define GPIO_A_RESET (0x1UL << 16)
+
+int main() {
+
+	volatile unsigned long *RCC_AHBENR_POINTER = (volatile unsigned long *) RCC_AHBENR;
+
+	*RCC_AHBENR_POINTER |= GPIO_A_CLOCK_ENABLE;
+
+	volatile unsigned long *GPIO_A_MODER_POINTER = (volatile unsigned long *) GPIO_A_MODER;
+
+	*GPIO_A_MODER_POINTER |= 0x1UL;
+
+	volatile unsignned long *GPIO_BSRR_POINTER = (volatile unsigned long *) GPIO_BSRR;
+
+	while (1) {
+		*GPIO_BSRR_POINTER |= 0x1UL;
+		for (volatile int i = 0; i < 150000; i++) {
+		};
+		*GPIO_BSRR_POINTER |= GPIO_A_RESET;
+		for (volatile int i = 0; i < 150000; i++) {
+		};
+	};
+};
